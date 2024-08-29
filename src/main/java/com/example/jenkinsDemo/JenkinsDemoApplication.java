c98ae3351f2d597c37c681d5610da5773405dd0f
SHOFIUDDIN <md.shofiuddin@bjitgroup.com>
2024-08-29 14:13:22 +0600

    Added Logger

--- src/main/java/com/example/jenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkinsDemo/JenkinsDemoApplication.java
@@ -1,12 +1,16 @@
 package com.example.jenkinsDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
+	static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	public static void main(String[] args) {
+		logger.info("This is a running job");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
